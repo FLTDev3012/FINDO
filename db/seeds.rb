c6c0cfab4de09a42ce921d21527e916a52d8505a
Eva Tchidemian <e.tchidemian@yahoo.fr>
2023-03-09 12:20:58 +0100

    ajout de la colonne photo a gift

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,19 @@ User.destroy_all
 
 dorian = User.create(email: "dorian@gmail.com", password: 'coucou')
 jernito = User.create(email: "jernito@gmail.com", password: 'coucou')
+eva = User.create(email: "eva@gmail.com", password: 'coucou')
+mathieu = User.create(email: "mathieu@gmail.com", password: 'coucou')
 
 wishlist_1 = Wishlist.create(name: 'Wishlist 1', user_id: dorian.id)
+
+
+# seeds des cadeaux
+# create_table "gifts", force: :cascade do |t|
+#   t.string "name"
+#   t.float "price"
+#   t.string "description"
+#   t.float "rating"
+#   t.string "url"
+#   t.integer "vote"
+
+Gift.create(name: 'Bouquet de fleurs', price: 25.99, description: 'Un joli bouquet de roses et de lys', rating: 3.4, url: "", vote: 0, photo: 'bouquet.jpg')
