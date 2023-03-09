2fe3e3f01eb1533533325435e0d4756d39483209
Eva Tchidemian <e.tchidemian@yahoo.fr>
2023-03-09 15:47:39 +0100

    fun

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # Wishlist.destroy_all
 User.destroy_all
 
-dorian = User.create(email: "dorian@gmail.com", password: 'coucou')
-jernito = User.create(email: "jernito@gmail.com", password: 'coucou')
-eva = User.create(email: "eva@gmail.com", password: 'coucou')
-mathieu = User.create(email: "mathieu@gmail.com", password: 'coucou')
+dorian = User.create(email: "dorian@gmail.com", password: 'coucou', username: "Dorian l'éléphant")
+jernito = User.create(email: "jernito@gmail.com", password: 'coucou', username: 'Jernito le rigolo')
+eva = User.create(email: "eva@gmail.com", password: 'coucou', username: 'Eva la catha')
+mathieu = User.create(email: "mathieu@gmail.com", password: 'coucou', username: 'Mathieu le mieleux')
 
 wishlist_1 = Wishlist.create(name: 'Anniversaire Jernito', user_id: dorian.id)
 
