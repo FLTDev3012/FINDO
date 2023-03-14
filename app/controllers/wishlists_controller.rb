4dff6a1cc4048e2fb2ab8bb79eb342d02c377c62
mathieu ralambotsirofo <m.ralambotsirofo@gmail.com>
2023-03-14 10:17:41 +0100

    wishilist_controller_vote_create

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -9,8 +9,12 @@ class WishlistsController < ApplicationController
     @wishlist = Wishlist.find(params[:id])
     # filter les gifts en fonction des tags # récupérer la valeur du champ de saisie
     @tags = @wishlist.tags
+    @gifts = []
+    WishlistsGift.where(wishlist: @wishlist).each do |wishlistgift|
+     @gifts << Gift.find(wishlistgift.gift_id)
+    end
     # @gifts = Gift.all.select { |gift| gift.tags.include?(@tags) }
-    @gifts = Gift.joins(:tags).where(tags: { id: @tags.pluck(:id) }).distinct
+    #@gifts = Gift.joins(:tags).where(tags: { id: @tags.pluck(:id) }).distinct
     # Gift.joins(:tags) : Cette partie de la ligne de code effectue une jointure entre les tables gifts et tags. En utilisant joins, nous demandons à ActiveRecord d'effectuer une jointure interne entre les tables en fonction de leur relation, ce qui signifie que seuls les cadeaux ayant au moins un tag seront inclus dans la requête.
     # Nous utilisons :tags pour spécifier la relation entre les modèles Gift et Tag. where(tags: { id: @tags.pluck(:id) }) : Cette partie de la ligne de code applique un filtre à la requête en utilisant les ID des tags de la variable @tags. Nous utilisons pluck(:id) pour extraire un tableau contenant les ID des tags, puis nous construisons une condition de filtre en utilisant ces ID. Nous spécifions la table tags et l'attribut id dans la condition en utilisant tags: { id: ... }
     # distinct : Cette partie de la ligne de code supprime les doublons de la requête. Si un cadeau a plusieurs tags correspondant aux ID filtrés, il apparaîtra une fois dans la collection @gifts
@@ -46,6 +50,12 @@ class WishlistsController < ApplicationController
     @chosen_tag = params.select { |key, value| value == "true" }.keys
     @chosen_tag.each do |tag|
       WishlistsTag.create!(wishlist: @wishlist, tag: Tag.find_by_name(tag))
+    end
+
+    @tags = @wishlist.tags
+    @gifts = Gift.joins(:tags).where(tags: { id: @tags.pluck(:id) }).distinct
+    @gifts.each do |gift|
+      WishlistsGift.create(wishlist: @wishlist, gift: gift, vote: 0)
     end
 
     authorize @wishlist
