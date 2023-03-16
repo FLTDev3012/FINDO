79b5d213b70ef03afcc42827730b7c1377c9e890
dorian fiolet <dorian3012@hotmail.fr>
2023-03-16 10:21:43 +0100

    endof2pagesdash

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "dashboard", to: "pages#dashboard"
+  get "dashboardwish", to: "pages#dashboardwish"
   resources :wishlists
 
   resources :wishlists_gifts, only: [:show]
