95d69b08da2219ab667f44380ad8e950a4ef6945
mathieu ralambotsirofo <m.ralambotsirofo@gmail.com>
2023-03-16 18:15:53 +0100

    copy to clipboard update

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -1,38 +1,38 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="share"
 export default class extends Controller {
-  static values = ['urlValue']
+  static values = ["urlValue"];
 
   connect() {
-    console.log("Coucou from Alert")
+    console.log("Coucou from Alert");
   }
 
   share(event) {
-    console.log(this.data.get("urlValue"))
+    console.log(this.data.get("urlValue"));
+    this.unsecuredCopyToClipboard(this.data.get("urlValue"));
+
+    Swal.fire({
+      icon: "success",
+      title: "Le lien a bien été copié",
+      text: this.data.get("urlValue"),
+      showConfirmButton: false,
+      timer: 40000,
+    });
+  }
+
+  unsecuredCopyToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
 
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
     try {
-      document.execCommand('copy');
+      document.execCommand("copy");
     } catch (err) {
-      console.error('Unable to copy to clipboard', err);
+      console.error("Unable to copy to clipboard", err);
     }
     document.body.removeChild(textArea);
-    Swal.fire({
-      icon: 'success',
-      title: 'Le lien a bien été copié',
-      text: this.data.get("urlValue"),
-      showConfirmButton: false,
-      timer: 40000
-    })
   }
-
-
-  function unsecuredCopyToClipboard(text) {
-  const textArea = document.createElement("textarea");
-  textArea.value = text;
-
-}
 }
